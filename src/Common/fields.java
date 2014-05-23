2b056037a42192fde0acb9a5c354e121e58ea6dc
unknown <bhargavpatel@cb5109-pc01.labs.sce.carleton.ca>
2014-05-23 12:15:30 -0400

    Initial Commit

---
+++ src/Common/fields.java
@@ -0,0 +1,11 @@
+package Common;
+
+
+/**
+ * Defines static fields that are to be used
+ * @author bhargavpatel
+ *
+ */
+public class fields {
+	
+}
---
+++ src/ErrorSim/TFTPHost.java
@@ -0,0 +1,5 @@
+package ErrorSim;
+
+public class TFTPHost {
+	
+}
